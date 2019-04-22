2346415f569afa7409f1494a1bf384cf94badeee
“ericliun <liunianbest@163.com>
2019-04-22 11:39:09 +0800

    search funtion

--- fisher.py
+++ fisher.py
@@ -1,22 +1,22 @@
 from flask import Flask, make_response
+from helper import is_isbn_or_key
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route('/hello')
-def hello():
-    # status code 200,404,301
-    # content-type http headers
-    # content-type = text/html
-    # Response
-    headers = {
-        'content-type':'text/plain',
-        'location':'http://www.bing.com'
-    }
-    # response= make_response('<html></html>', 301)
-    # response.headers = headers
-    return '<html></html>', 301, headers
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    '''
+    q:普通关键字 isbn
+    page:
+    :return:
+    '''
+    # isbn isbn13 13个0到9的数字组成
+    # isbn10 10个0到9的数字组成，含有一些'-'
+    isbn_or_key = is_isbn_or_key(q)
+
+    pass
 
 
 def helloo():
